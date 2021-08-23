fbe1741a3a47a658e500e5b182b38ae436601190
Charonyx <churairat.onn@gmail.com>
2021-08-24 02:35:20 +0700

    pandas basic

--- Lab2/PandasLibrary.py
+++ Lab2/PandasLibrary.py
@@ -22,10 +22,20 @@ import pandas as pd
 
 data = {
   "calories": [420, 380, 390],
-  "duration": [50, 40, 45]
+  "duration": [50, 40, 45],
+  "name"    : ["A","B","C"]
 }
 
 #load data into a DataFrame object:
 df = pd.DataFrame(data)
+df2 = pd.DataFrame(data, index = ["day1", "day2", "day3"])
+print(df, end='\n------------------\n')
+print(df2, end='\n------------------\n') 
+# print(df.loc[0].keys)
+print(df.loc[0], end='\n------------------\n')
+print(df.loc[1], end='\n------------------\n')
+print(df.loc[[0, 1]], end='\n------------------\n')
 
-print(df) 
+# df = pd.read_csv('data.csv')
+
+# https://www.w3schools.com/python/pandas/pandas_dataframes.asp

0132bd6604098aff67ba3508e48cdc6edfeaae2e
Charonyx <churairat.onn@gmail.com>
2021-09-07 23:21:13 +0700

    color crush

--- chap03/chap03_3colorCrush.py
+++ chap03/chap03_3colorCrush.py
@@ -21,56 +21,50 @@ class Stack:
     def getStackIndex(self, index) :
         return self.color[index]
     def __str__(self) :
-        return [print(i, end='') for i in self.color]
+        return [print(self.getStackIndex(i), end='') for i in range(self.size() -1, -1, -1)]
 
-# def crush() :
-    
-
-color = input('Enter Input : ').split()
-stack = Stack()
-[stack.push(i) for i in color]
-
-
-while 1 :
+if __name__ == '__main__' :
+    color = input('Enter Input : ').split()
+    stack = Stack()
+    [stack.push(i) for i in color]
     combo = 0
-    crush = False
-    nowColor = stack.peek()
+    loopAgain = True
     stack2 = Stack()
-    for i in range(stack.size() -1, -1, -1) : # range(stack.size() -1, -1, -1)
-        # if color[i] == color[i - 1] == color[i - 2] :
-        if stack.getStackIndex(i) == stack.getStackIndex(i - 1) and stack.getStackIndex(i - 1) == stack.getStackIndex(i - 2) and i >= 2 :
-            combo += 1
-            # pop 
-        else :
-            stack2.push(stack.getStackIndex(i))
-        stack.pop()
-    print(stack.getStack())
-    print(stack2.getStack())
-
-    for i in range(stack2.size() -1, -1, -1) :
-        stack.push(stack2.getStackIndex(i))
-        stack2.pop()
-    print(stack.getStack())
-    print(stack2.getStack())
-
-    break
-        # if len(color) >= 3 and color[i] = color[i-1]
-            
-#         S.push(i)
-
-
-
-print(stack.getStack())
-print(stack2.getStack())
-
-print(stack.size())
-stack.__str__()
+    while loopAgain :
+        nowColor = stack.peek()
+        j = int()
+        for i in range(stack.size() -1, 0, -1) :
+            if i == j - 1 or i == j - 2 : 
+                stack.pop()
+                continue
+            if stack.getStackIndex(i) == stack.getStackIndex(i - 1) == stack.getStackIndex(i - 2) and (not stack.getStackIndex(i) == stack.getStackIndex(-1) or not i == 1) :
+                combo += 1
+                j = i
+                if stack.size() == 3 : stack.pop()  
+            else : stack2.push(stack.getStackIndex(i))
+            stack.pop()
+        for i in range(stack2.size() -1, -1, -1) :
+            stack.push(stack2.getStackIndex(i))
+            stack2.pop()
+        for i in range(stack.size() -1, 0, -1) :
+            if stack.getStackIndex(i) == stack.getStackIndex(i - 1) == stack.getStackIndex(i - 2) :
+                loopAgain = True
+                break
+            else : loopAgain = False
+        if stack.size() <= 1 : break
+    # print(stack.getStack())
+    # print(stack2.getStack())
+    print(stack.size())
+    if stack.getStack() == []   : print('Empty')
+    else                        : 
+        stack.__str__()
+        print()
+    if combo >= 2               : print(f'Combo : {combo} ! ! !')
 
 '''
 Enter Input : G H H H H P
 3
 PHG
-
 
 Enter Input : L C C X X X C D
 2
